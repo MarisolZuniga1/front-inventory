c1cb47bffa0e9855ed8576cc36e4a7ebede4efa7
MarisolZuniga1 <z.v.r.marisol@gmail.com>
2024-05-22 12:44:00 -0600

    sidenlav

--- src/app/modules/dashboard/dashboard.module.ts
+++ src/app/modules/dashboard/dashboard.module.ts
@@ -1,18 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { DashboardComponent } from './pages/dashboard.component';
 import { HomeComponent } from './components/home/home.component';
-import { RouterModule } from '@angular/router';
+import { DashboardComponent } from './pages/dashboard.component';
+import { SharedModule } from '../shared/shared.module';
 
 
 
 @NgModule({
   declarations: [
-    DashboardComponent, 
-    HomeComponent],
+    DashboardComponent,
+    HomeComponent
+  ],
   imports: [
     CommonModule,
-    RouterModule
+    SharedModule
   ]
 })
 export class DashboardModule { }
